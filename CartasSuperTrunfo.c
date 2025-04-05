080c2cb8a4f479d395b68509c548dd6945d0ddfa
luanllian <luanjoselian@gmail.com>
2025-04-05 15:47:50 +0000

    Cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,39 @@
  #include <stdio.h>
 
-  int main(){
+  int main() {
     printf("Desafio Xadrez! \n");
+
+int main() {
+  printf("Carta 1! \n");
+  printf("Estado: ");
+scanf("%d, &A");
+printf("código: ");
+scanf("%d, &A01");
+printf("Nome da Cidade: ");
+scanf("%d, &São Paulo");
+printf("População: ");
+scanf("%d, &12325000");
+printf("Area: ");
+scanf("%d, &1521.11 km");
+printf("PIB: ");
+scanf("%d, &699.28bilhões");
+printf("Número de Pontos Turísticos: ");
+scanf("%d, &50");
+
+int main() {
+  printf("Carta 2! \n");
+  printf("Estado: ");
+scanf("%d, &B");
+printf("código: ");
+scanf("%d, &B02");
+printf("Nome da Cidade: ");
+scanf("%d, &Rio de Janeiro");
+printf("População: ");
+scanf("%d, 6748000");
+printf("Area: ");
+scanf("%d, &1200.25 km");
+printf("PIB: ");
+scanf("%d, &300.50bilhões");
+printf("Número de Pontos Turísticos: ");
+scanf("%d, &30");
+
